07d28b7d4ccbf25fb022435f3ca47dd678ccb9b9
castawaypirate <apsikiotis@gmail.com>
2025-10-17 20:53:01 +0300

    add nvim code action keybinding and dotfiles alias

--- .bashrc
+++ .bashrc
@@ -7,6 +7,7 @@
 
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
+alias dotfiles='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 PS1='[\u@\h \W]\$ '
 
 export NVM_DIR="$HOME/.nvm"
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Manual code action keybinding as fallback
+vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code Action' })
