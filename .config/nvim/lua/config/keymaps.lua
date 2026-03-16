bfdda84bc3f8279cb182a213491b742deddd9f9a
castawaypirate <apollonas@csd.auth.gr>
2026-03-16 19:48:07 +0200

    neovim kickstart for web and c++ dev

--- .config/nvim/lua/config/keymaps.lua
+++
@@ -1,6 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
--- Manual code action keybinding as fallback
-vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code Action' })
